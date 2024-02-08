959d3b6a39162964b0cb0d78b9261b01078f982e
Zbynek Svoboda <wentura17@seznam.cz>
2024-02-08 21:16:01 +0100

    matomo

--- app/components/matomo.js
+++ app/components/matomo.js
@@ -1,21 +1,36 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Matomo() {
-  useEffect(() => {
-    var _paq = (window._paq = window._paq || []);
-    _paq.push(["trackPageView"]);
-    _paq.push(["enableLinkTracking"]);
-    (function () {
-      var u = "http://matomo.zbyneksvoboda.cz/";
-      _paq.push(["setTrackerUrl", u + "matomo.php"]);
-      _paq.push(["setSiteId", "1"]);
-      var d = document,
-        g = d.createElement("script"),
-        s = d.getElementsByTagName("script")[0];
-      g.async = true;
-      g.src = u + "matomo.js";
-      s.parentNode.insertBefore(g, s);
-    })();
-  });
+  return (
+    <div>
+      <img
+        referrerPolicy="no-referrer-when-downgrade"
+        src="http://matomo.zbyneksvoboda.cz/matomo.php?idsite=8&amp;rec=1"
+        className="border-0"
+        alt=""
+      />
+    </div>
+  );
 }
+
+// "use client";
+// import React, { useEffect } from "react";
+
+// export default function Matomo() {
+//   useEffect(() => {
+//     var _paq = (window._paq = window._paq || []);
+//     _paq.push(["trackPageView"]);
+//     _paq.push(["enableLinkTracking"]);
+//     (function () {
+//       var u = "//matomo.zbyneksvoboda.cz/";
+//       _paq.push(["setTrackerUrl", u + "matomo.php"]);
+//       _paq.push(["setSiteId", "8"]);
+//       var d = document,
+//         g = d.createElement("script"),
+//         s = d.getElementsByTagName("script")[0];
+//       g.async = true;
+//       g.src = u + "matomo.js";
+//       s.parentNode.insertBefore(g, s);
+//     })();
+//   });
+// }
